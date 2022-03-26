7535cfed4e47768ecd6b343a7e67787cf7b106f5
YOUNG-BLUE <youngblue.coding@gmail.com>
2022-03-26 15:27:27 +0900

    상품 항목들의 정보 가져오기  * 아직 상품이 응모 가능한 상품인지 아닌지 조건 안넣음

--- src/main/java/bluemoon/nikedrawalertbot/jsoup/nikeWeb/NikeWebJsoup.java
+++ src/main/java/bluemoon/nikedrawalertbot/jsoup/nikeWeb/NikeWebJsoup.java
@@ -1,6 +1,10 @@
 package bluemoon.nikedrawalertbot.jsoup.nikeWeb;
 
 import java.io.IOException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -15,26 +19,30 @@ import org.springframework.stereotype.Component;
 //@Component
 public class NikeWebJsoup {
 
-    public String getTodayDrawList() throws IOException {
+    public String getTodayDrawList() {
 
         final String drawList = "https://www.nike.com/kr/launch/?type=upcoming";
         Connection conn = Jsoup.connect(drawList);
-        Document document = conn.get();
-//        try{
-//            List<Element> drawShoesList = new ArrayList<>();
-//            Document document = conn.get();
-//            Elements elements = document.select("[data-active-date]");
-//            for(Element e : elements) {
-//                if(e.text().contains("응모 시작")){
-//                    drawShoesList.add(e);
-//                }
-//            }
-//        } catch(Exception e) {
-//            log.error(String.valueOf(e));
-//        }
-//
-//        return drawList;
-        System.out.println(document);
+        Document document = null;
+        try {
+            document = conn.get();
+            Elements contents = document.select(".figcaption-content");
+
+            if (contents.size() > 0) {
+                // 가져온 상품명 목록 태그로부터 상품명, 출시시간, 상품링크를 추출
+                for (Element e : contents) {
+                    System.out.println(e.getElementsByClass("headline-3").text());
+                    System.out.println(e.getElementsByClass("headline-5").text());
+                    System.out.println("https://www.nike.com/" + e.select("a").attr("href"));
+                    System.out.println("");
+                }
+            } else {
+                log.info(new SimpleDateFormat("yyyy-MM-dd").format(ZonedDateTime.now(ZoneId.of("Asia/Seoul")))
+                         + " - 응모 가능한 상품이 없습니다.");
+            }
+        } catch (IOException ie) {
+            log.error(String.valueOf(ie));
+        }
         return String.valueOf(document);
     }
 
