da9d3941c06411a8268767cef10dba81d2b27c7f
YOUNG-BLUE <youngblue.coding@gmail.com>
2022-03-30 22:01:30 +0900

    스케쥴 작업 추가

--- src/main/java/bluemoon/nikedrawalertbot/NikeDrawAlertBotApplication.java
+++ src/main/java/bluemoon/nikedrawalertbot/NikeDrawAlertBotApplication.java
@@ -5,16 +5,18 @@ import bluemoon.nikedrawalertbot.telegram.SendMessage;
 import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class NikeDrawAlertBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NikeDrawAlertBotApplication.class, args);
-        NikeWebJsoup nikewJs = new NikeWebJsoup();
-        List<String> todayDrawList = nikewJs.getTodayDrawList();
+//        NikeWebJsoup nikewJs = new NikeWebJsoup();
+//        List<String> todayDrawList = nikewJs.getTodayDrawList();
 
         SendMessage telegram = new SendMessage();
-        telegram.sendTelegram(todayDrawList);
+        telegram.sendTelegram();
     }
 }
--- src/main/java/bluemoon/nikedrawalertbot/telegram/SendMessage.java
+++ src/main/java/bluemoon/nikedrawalertbot/telegram/SendMessage.java
@@ -1,17 +1,26 @@
 package bluemoon.nikedrawalertbot.telegram;
 
 import bluemoon.nikedrawalertbot.config.Config;
+import bluemoon.nikedrawalertbot.jsoup.nikeWeb.NikeWebJsoup;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SendMessage {
 
-    public void sendTelegram(List<String> drawList) {
+    @Scheduled(cron = "0 0 9 * * *")
+    public void sendTelegram() {
+        // 이렇게 작성하면 의존성이 생기지만 @Scheduled를 사용하기 위해 적용
+        NikeWebJsoup nike = new NikeWebJsoup();
+        List<String> drawList = nike.getTodayDrawList();
+
         Config config = new Config();
 
         String myToken = config.getToken();
@@ -28,6 +37,11 @@ public class SendMessage {
                 conn.setRequestMethod("GET");
                 bf = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
 
+                // 응답 출력
+                String line;
+                while((line = bf.readLine()) != null) {
+                    System.out.println(line);
+                }
             }
         } catch(Exception e) {
             e.printStackTrace();
